7796c6c28ba7e1a0534d38d7eb9c0f83f72b204c
unnawut <unnawut@unnawut.com>
2025-01-31 17:12:37 +0700

    process_block_header() through the zkVM

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,16 +1,28 @@
 use risc0_zkvm::guest::env;
-use beam_types::BeamState;
+use ream_consensus::deneb::beacon_state::BeaconState;
+use ream_consensus::deneb::beacon_block::BeaconBlock;
 
 mod beacon_chain;
 
 fn main() {
-    // TODO: Implement your guest code here
+    let count_start = env::cycle_count();
 
-    // Read the input
-    let pre_state: BeamState = env::read();
+    // Read the pre-state
+    let mut pre_state: BeaconState = env::read();
+    let count_after_read_prestate = env::cycle_count();
 
-    // TODO: Transition the state
-    let post_state = pre_state;
+    // Read the block
+    let block: BeaconBlock = env::read();
+    let count_after_read_block = env::cycle_count();
+
+    // Transition the state
+    let post_state = pre_state.process_block_header(block).unwrap();
+    let count_after_process_block_header = env::cycle_count();
+
+    // Benchmark cycle counts
+    eprintln!("read_pre_state: {}", count_after_read_prestate - count_start);
+    eprintln!("read_block: {}", count_after_read_block - count_after_read_prestate);
+    eprintln!("process_block_header: {}", count_after_process_block_header - count_after_read_block);
 
     // Write public output to the journal
     env::commit(&post_state);
